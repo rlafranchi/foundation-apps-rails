a2ba540302e9473394690e99c6d48b12e6928b42
Richard LaFranchi <rlafranchi@serenethemes.com>
2014-12-26 16:13:08 -0700

    precompile

---
+++ public/assets/build/routes-7b1f2e2dde5742ae3f8052fffae322a1.js
@@ -0,0 +1,1 @@
+var foundationRoutes = [{"name":"posts","url":"/posts","controller":"PostsController","path":"templates/posts.html"},{"name":"posts.show","url":"/:id","path":"templates/posts.show.html"},{"name":"home","url":"/","path":"templates/home.html"}];
