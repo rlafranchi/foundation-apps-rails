91fb96310ed7c9694e9618b458fc1c616bf00805
Richard LaFranchi <rlafranchi@serenethemes.com>
2015-05-09 09:32:18 -0600

    update app and include templates

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,7 +1,36 @@
-var app = angular.module('application');
+(function() {
+  'use strict';
 
-app.config([ '$httpProvider', '$locationProvider', function($httpProvider, $locationProvider) {
+  angular.module('application', [
+    'ui.router',
+    'ngAnimate',
+
+    //foundation
+    'foundation',
+    'foundation.dynamicRouting',
+    'foundation.dynamicRouting.animations'
+  ])
+    .config(config)
+    .run(run)
+  ;
+
+  config.$inject = ['$urlRouterProvider', '$locationProvider'];
+
+  function config($urlProvider, $locationProvider, $httpProvider) {
+    $urlProvider.otherwise('/');
+
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: true
+    });
+
     $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
-    $locationProvider.html5Mode({ enabled: true, requireBase: true });
   }
-]);
+
+
+  function run() {
+    FastClick.attach(document.body);
+  }
+
+  var app = angular.module('application');
+})();
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,3 +8,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( build/foundation.js )
 Rails.application.config.assets.precompile += %w( build/routes.js )
+Rails.application.config.assets.precompile += %w( build/templates.js )
